d3099f168ca507a9510c384a9478e798488b4892
ZZbeta <1916948871@qq.com>
2016-06-15 17:10:34 +0800

    update in 6.15
    根据 wang 的 java 设计修改，包括了两个验证规则

--- validata6.7.py
+++ validata6.7.py
@@ -1,48 +1,58 @@
 from pyspark import SparkContext
-sc=SparkContext(appName="validation")
+sc=SparkContext(appName="validata6.7")
+
 import datetime
 import dateutil.parser as par
 
-def tryparse(date):
+def parse(date):
     kwargs={}
-    try:
-        parsedate=par.parse(date,**kwargs)
-    except ValueError:
-        parsedate='icannt'
-    if parsedate != 'icannt':
-        parsedate=parsedate.strftime('%d-%m-%Y')
-        return parsedate
-    else:
-        return date
+    parsedate=par.parse(date,**kwargs)
+    return parsedate
 
 class MetaFileHandler:
-    def __init__(self, file_name):
+
+    def __init__(self,file_name):
         self.file_name=file_name
-    def meta_kv_mapper(self, x):
-        return (x[0], x[2]),(x[1],x[4])
-    def meta_validate_fields(self, x):
+        self.rules=['<= #NOW','!IN (DayOfWeek(),(1,7))']
+        self.NOW=datetime.datetime.now()
+
+    def meta_kv_mapper(self,x):
+        x=x.strip().split(' ')
+        return x
+
+    def validate_on_rules(self,rule,time):
         err = ''
-        i=0
-        time1=tryparse(x[1][0])
-        time2=tryparse(x[1][1])
-        if len(time1)==9:
-            i=6
-        else:
-            i=4
-        if(time1[i:]<'2015'):
-            err = err + 'field 1 date is too old'
-        if(time2[i:]<'2015'):
-            err = err + 'field 4 date is too old'
-        return (x[0], err)
-    def meta_kv_to_records(self, x):
-        return [x[0][0], x[0][1], x[1]]
+        if rule == '<= #NOW':
+            if time > self.NOW:
+                err = 'Greater than now; '
+        elif rule == '!IN (DayOfWeek(),(1,7))':
+            if time.weekday() == 0 or time.weekday() == 6:
+                err = 'Date is monsday or sunday;'
+        return err
+
+    def meta_validate_fields(self,x):
+        err=''
+        time=parse(x[2])
+        for rule in self.rules:
+            err = err + self.validate_on_rules(rule,time)
+        x[2]=time.strftime('%d-%m-%Y')
+        return x[0],x[1],x[2],err
+
+    def meta_to_str(self,x):
+        return (' ').join((x[0],x[1],x[2],x[3],'\n'))
 
 meta = MetaFileHandler('testdata.txt')
-rdd=sc.textFile(meta.file_name)
-rdd=rdd.map(lambda x:x.split(' '))
+
+rdd = sc.textFile(meta.file_name)
+
 rdd_kv = rdd.map(lambda x: meta.meta_kv_mapper(x))
-rdd_kv.collect()
-rdd_kv_invalid = rdd_kv.map(lambda x: meta.meta_validate_fields(x)).filter(lambda x: x[1]     > '')
-rdd_kv_invalid.collect()
-rdd_error=rdd_kv_invalid.flatMap(lambda x: meta.meta_kv_to_records(x))
-print rdd_error.collect()
+
+rdd_kv_invalid = rdd_kv.map(lambda x: meta.meta_validate_fields(x)).filter(lambda x: x[3] > '')
+
+rdd_to_str = rdd_kv_invalid.map(lambda x: meta.meta_to_str(x))
+
+f = open('err_data.txt','w')
+
+data = rdd_to_str.reduce(lambda a,b: a+b)
+
+f.write(data)
