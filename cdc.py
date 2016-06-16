3d891f913935e870f667cd1847947b963cb38117
JiangDoudou1994 <314099829@qq.com>
2016-06-16 16:50:07 +0800

    （运行后代码）处理数据格式，使得例如：1.0=1月日年=年月日；单元测试代码在主程序中删除，另外导入

--- cdc.py
+++ cdc.py
@@ -4,6 +4,11 @@ sc=SparkContext(appName="cdc")
 import datetime
 import dateutil.parser as par
 
+def parse(date):
+    kwargs={}
+    parsedate=par.parse(date,**kwargs)
+    return parsedate
+
 class MetaFileHandler:
     
     def __init__(self, file_name):
@@ -12,30 +17,26 @@ class MetaFileHandler:
     def meta_kv_mapper(self, x):
         return (x[0], x[2]),(x[1],x[4])
         
-    def parse(date):
-        kwargs={}
-        parsedate=par.parse(date,**kwargs)
-    return parsedate
-    
     def handle_data(self,x):
-        x[1][0]=time.strftime('%d-%m-%Y')
-        x[1][1]=time.strftime('%d-%m-%Y')
+        time1=parse(x[1][0])
+        time2=parse(x[1][1])
+        x[1][0]=time1.strftime('%d-%m-%Y')
+        x[1][1]=time2.strftime('%d-%m-%Y')
         return (x[0][0].split('.')[0],x[0][1].split('.')[0]),(x[1][0],x[1][1])
 
 meta1 = MetaFileHandler('testdata1.txt')
 meta2 = MetaFileHandler('testdata2.txt')
-rdd1=sc.textFile(meta1.file_name)
-rdd2=sc.textFile(meta2.file_name)
-rdd1_kv = rdd1.map(lambda x: meta1.meta1_kv_mapper(x))
-rdd2_kv = rdd2.map(lambda x: meta2.meta2_kv_mapper(x))
-rdd3_kv = rdd3.map(lambda x: meta3.handle_data(x))
-rdd1_kv.collect()
-rdd2_kv.collect()
-rdd_combo = rdd1_kv.fullOuterJoin(rdd2_kv)
+rdd1 = sc.textFile(meta1.file_name)
+rdd2 = sc.textFile(meta2.file_name)
+rdd1_kv = rdd1.map(lambda x: meta1.meta_kv_mapper(x))
+rdd2_kv = rdd2.map(lambda x: meta2.meta_kv_mapper(x))
+rdd1_handle = rdd1_kv.map(lambda x: meta1.handle_data(x))
+rdd2_handle = rdd2_kv.map(lambda x: meta2.handle_data(x))
+rdd_combo = rdd1_handle.fullOuterJoin(rdd2_handle)
 rdd_combo.collect()
 rdd_del = rdd_combo.filter(lambda x: x[1][0] == None)
 rdd_add = rdd_combo.filter(lambda x: x[1][1] == None)
 rdd_chg = rdd_combo.filter(lambda x: x[1][0] != None and x[1][1] != None and x[1][0] != x[1][1])
 rdd_del.collect()
 rdd_add.collect()
-rdd_chg.collect()
+print rdd_chg.collect()
