33cc173581b50b72c2782c5cc2dcea50d19ff3ef
ZZbeta <1916948871@qq.com>
2016-06-17 17:11:37 +0800

    fix two errors according to unittest

--- validate.py
+++ validate.py
@@ -41,7 +41,7 @@ class MetaFileHandler:
             return ''
 
     def validate_fdm(self, time, day_add=5):
-        day_of_time = time.strftime('%d')
+        day_of_time = int(time.strftime('%d'))
         if day_of_time <= day_add:
             return 'Date is earlier than 5th; '
         else:
@@ -51,7 +51,7 @@ class MetaFileHandler:
         day_of_time = int(time.strftime('%d'))
         weekday_of_time = time.weekday()
         if day_of_time <= 7:
-            if weekday_of_time <= day_of_time - 1 and weekday_of_time >= 0:
+            if weekday_of_time + 1 >= day_of_time:
                 return 'Date is earlier than FMM; '
             else:
                 return ''
