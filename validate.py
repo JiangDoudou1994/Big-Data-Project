396d76d0fd28a82b21598bc3a4042aa6e35efa22
ZZbeta <1916948871@qq.com>
2016-06-17 11:47:32 +0800

    Rebuild the rule functions. New rule functions accept more variables.

--- validate.py
+++ validate.py
@@ -11,73 +11,73 @@ class MetaFileHandler:
 
     def __init__(self,file_name):
         self.file_name = file_name
-	self.rules = ['>= 01/01/1890','<= #NOW','!IN (DayOfWeek(),(1,7))',
-	            '#FDM,DateAdd(D,5,#FDM','>= #FMM']
+        self.rules = ['>= oldest','<= #NOW','!IN (DayOfWeek())',
+	              '#FDM,DateAdd()','>= #FMM']
 
     def meta_kv_mapper(self,x):
         x = x.strip().split(' ')
-	return x
+        return x
 
-    def validate_oldest(self,time):
-        oldest_time = parse('01-01-1890')
-	if time < oldest_time:
-	    return 'Earlier than odest time'
-	else:
-	    return ''
+    def validate_oldest(self,time,oldest='01-01-1890'):
+        oldest_time = parse(oldest)
+        if time < oldest_time:
+            return 'Earlier than odest time'
+        else:
+            return ''
 
-    def validate_now(self,time):
-        now = parse('16-06-2016')
-	if time > now:
-	    return 'Greater than now'
-	else:
-	    return ''
+    def validate_now(self,time,now='16-06-2016'):
+        now = parse(now)
+        if time > now:
+            return 'Greater than now'
+        else:
+            return ''
 
-    def validate_mon_or_sun(self,time):
-        if time.weekday() == 0 or time.weekday() == 6:
-            return 'Date is monday or sunday;'
-	else:
-	    return ''
+    def validate_weekdays(self,time,weekdays=[0,6]):
+        if time.weekday() in weekdays:
+            return 'Date is in invalid;'
+        else:
+            return ''
 
-    def validate_fdm(self,time):
+    def validate_fdm(self,time,day_add=5):
         day_of_time = time.strftime('%d')
-	if day_of_time <= '5':
-	    return 'Date is earlier than 5th;'
-	else:
-	    return ''
+        if day_of_time <= day_add:
+            return 'Date is earlier than 5th;'
+        else:
+            return ''
 
     def validate_fmm(self,time):
         day_of_time = int(time.strftime('%d'))
-	weekday_of_time = time.weekday()
-	if day_of_time <= 7:
-	    if weekday_of_time <= day_of_time-1 and weekday_of_time >= 0:
-	        return 'Date is earlier than FMM'
-	    else:
-	        return ''
-	else:
-	    return ''
+        weekday_of_time = time.weekday()
+        if day_of_time <= 7:
+            if weekday_of_time <= day_of_time-1 and weekday_of_time >= 0:
+                return 'Date is earlier than FMM'
+            else:
+                return ''
+        else:
+            return ''
 
     def validate_on_rules(self,rule,time):
         err = ''
-	if rule == '>= 01/01/1890':
-	    err = self.validate_oldest(time)
-	elif rule == '<= #NOW':
-	    err = self.validate_now(time)
-	elif rule == '!IN (DayOfWeek(),(1,7))':
-	    err = self.validate_mon_or_sun(time)
-	elif rule == '#FDM,DateAdd(D,5,#FDM':
-	    err = self.validate_fdm(time)
-	elif rule == '>= #FMM':
-	    err = self.validate_fmm(time)
-	return err
+        if rule == '>= oldest':
+            err = self.validate_oldest(time)
+        elif rule == '<= #NOW':
+            err = self.validate_now(time)
+        elif rule == '!IN (DayOfWeek())':
+            err = self.validate_weekdays(time)
+        elif rule == '#FDM,DateAdd()':
+            err = self.validate_fdm(time)
+        elif rule == '>= #FMM':
+            err = self.validate_fmm(time)
+        return err
             
 
     def meta_validate_fields(self,x):
         err=''
-	time=parse(x[2])
-	for rule in self.rules:
-	    err = err + self.validate_on_rules(rule,time)
-	x[2]=time.strftime('%d-%m-%Y')
-	return x[0],x[1],x[2],err
+        time=parse(x[2])
+        for rule in self.rules:
+            err = err + self.validate_on_rules(rule,time)
+        x[2]=time.strftime('%d-%m-%Y')
+        return x[0],x[1],x[2],err
 
     def meta_to_str(self,x):
         return (' ').join((x[0],x[1],x[2],x[3],'\n'))
