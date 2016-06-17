b6b184089b3f61ddfbe55cce41089095ed93c264
alex liu <atutest@gmail.com>
2016-06-17 10:31:19 -0500

    add automatic test and rule_parser_test

--- tests/rule_parser_test.py
+++ tests/rule_parser_test.py
@@ -5,10 +5,12 @@ from utils import rule_parser
 class rule_parser_test(unittest.TestCase):
 
     def setUp(self):
+
         self.test_class = rule_parser.rule_parser()
 
     def test_parse_placeholder(self):
+        pr=self.test_class.parse( '01/03/2016', '>= #FDM')
+        self.assertEqual(pr , '>= 01/01/2016',msg=pr)
 
-        self.assertTrue(self.test_class.parse('01/03/2016','>#FDM') == '>01/01/2016')
-if __name__=='__main__':
+if __name__ == '__main__':
     unittest.main()
--- utils/rule_parser.py
+++ utils/rule_parser.py
@@ -1,6 +1,15 @@
 import pyparsing
+from pyparsing import Word, alphas, OneOrMore,replaceWith,Dict,oneOf
+
 
 class rule_parser:
-    def parse(self,value,rule):
-        return
 
+    def parse(self, value, rule):
+        placeholder="#"+Word(alphas)
+        placeholder.setParseAction(replaceWith('01/01/2016')) 
+        expression=OneOrMore(oneOf("> = < >= <="))("expression")
+        final=expression + placeholder
+
+        result= final.parseString(rule)
+        
+        return ' '.join(result) 
