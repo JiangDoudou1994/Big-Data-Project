7ae82f2e522eba964fe43b0a8429ed4a1dbd2538
atutest <atutest@gmail.com>
2016-06-20 06:46:47 -0500

    refactoring validate

--- validate/validate.py
+++ validate/validate.py
@@ -1,57 +1,20 @@
+import sys
 from pyspark import SparkContext
-from rule_parser import rule_parser
 import datetime
-import validate_rules
+from metafile_handler import MetaFileHandler
 
 
-def parse(date):
-    kwargs = {}
-    parsedate = par.parse(date, **kwargs)
-    return parsedate
-
-
-class MetaFileHandler:
-
-    def __init__(self, file_name, rules):
-        self.file_name = file_name
-        self.rules = rules
-
-    def meta_kv_mapper(self, x):
-        x = x.strip().split(',')
-        rule_index = [int(r[0]) - 1 for r in self.rules]
-# only return id and column with rules
-        return x[0], ([x[index] for index in rule_index])
-
-    def validate_on_rules(self, value, rule):
-        rp = rule_parser()
-        rule = rp.parse(value, rule)
-        validate = validate_rules.Validate()
-        return validate.validate(value, rule)
-
-    def meta_validate_fields(self, x):
-        err = ''
-        index = 0
-        for rule in self.rules:
-            for u in rule[1].split(';'):
-                if(not self.validate_on_rules(x[1][index], u.encode('ascii')[1:-1])):
-                    err = err +" "+ x[1][index]+" should " +u
-            index += 1
-        return [x, err]
-
-    def meta_to_str(self, x):
-        return x
+argv=sys.argv
 
 sc = SparkContext(appName="validata")
-meta_data = sc.textFile("testdata.meta")
-rules = meta_data.filter(lambda x: len(x.split('||')) > 1).map(
-    lambda x: [int(x.split(',')[0]), x.split('||')[1]])
+meta_data = sc.textFile(argv[0])
 
-meta = MetaFileHandler('testdata.txt', rules.collect())
-rdd = sc.textFile(meta.file_name)
-rdd_kv = rdd.map(lambda x: meta.meta_kv_mapper(x))
+meta_handler = MetaFileHandler(meta_data)
+rdd = sc.textFile(argv[1])
+rdd_kv = rdd.map(lambda x: meta_handler.meta_kv_mapper(x))
 rdd_kv_invalid = rdd_kv.map(
-    lambda x: meta.meta_validate_fields(x)).filter(lambda x: x[1] > '')
+    lambda x: meta_handler.meta_validate_fields(x)).filter(lambda x: x[1] > '')
 rdd_to_str = rdd_kv_invalid.map(
-    lambda x: meta.meta_to_str(x))  
+    lambda x: meta_handler.meta_to_str(x))  
 now = datetime.datetime.now()
 rdd_to_str.saveAsTextFile('validate_error_{:%H_%M}.txt'.format(now))
